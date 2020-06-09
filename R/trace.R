4716dd11330964628664ab66bf1be4239cda742b
Aviral Goel <goel.aviral@gmail.com>
2020-06-09 10:39:51 -0400

    Add flag to enable/disable quoting of code block

--- R/trace.R
+++ R/trace.R
@@ -2,8 +2,10 @@
 #' @export
 #' @importFrom lightr trace_code
 #' @importFrom utils write.csv
-trace_eval <- function(code, envir=parent.frame(), datafile) {
-    code <- substitute(code)
+trace_eval <- function(code, quote=TRUE, envir=parent.frame(), datafile) {
+    if(quote) {
+        code <- substitute(code)
+    }
     context <- create_evil_context()
     result <- trace_code(code, context, envir)
     eval_data <- get_data(context)
