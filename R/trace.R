6125c6f68f7983f05c7cf2f9767568949f3218da
Aviral Goel <goel.aviral@gmail.com>
2020-06-09 11:20:07 -0400

    Write error message to file

--- R/trace.R
+++ R/trace.R
@@ -1,14 +1,37 @@
 
 #' @export
-#' @importFrom lightr trace_code
+#' @importFrom lightr trace_code is_error get_error get_source get_message get_call
 #' @importFrom utils write.csv
-trace_eval <- function(code, quote=TRUE, envir=parent.frame(), datafile) {
-    if(quote) {
+trace_eval <- function(code,
+                       datadir=file.path(getwd(), ".evil"),
+                       envir=parent.frame(),
+                       quote=TRUE) {
+    if (quote) {
         code <- substitute(code)
     }
+
     context <- create_evil_context()
-    result <- trace_code(code, context, envir)
+
+    result <- trace_code(code, context, envir, quote=FALSE)
+
     eval_data <- get_data(context)
-    write.csv(eval_data, datafile, row.names = FALSE)
+
+    ## create datadir
+    dir.create(datadir, showWarnings=FALSE)
+
+    ## write eval data
+    data_file_path <- file.path(datadir, "eval-calls.csv")
+    write.csv(eval_data, data_file_path, row.names = FALSE)
+
+    ## handle error
+    if (is_error(result)) {
+        status_file <- file.path(datadir, "ERROR")
+        error <- get_error(result)
+        error_data <- data.frame(source = get_source(error),
+                                 message = get_message(error),
+                                 call = paste(deparse(get_call(error)), sep="\n"))
+        write.csv(error_data, status_file, row.names=FALSE)
+    }
+
     result
 }
