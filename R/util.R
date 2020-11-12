a1768d59662024ba415997dcc462d86fa2fee385
Aviral Goel <goel.aviral@gmail.com>
2020-11-12 18:14:33 -0500

    Modify typeof to take expression element into account

--- R/util.R
+++ R/util.R
@@ -1,5 +1,10 @@
 sexp_typeof <- function(x)
-    .Call(C_sexp_typeof, x)
+    if(typeof(x) == "expression") {
+        paste(unlist(Map(sexp_typeof, x)), collapse = ";", sep = ";")
+    }
+    else {
+        .Call(C_sexp_typeof, x)
+    }
 
 mark_parsed_expression <- function(x, parse_fun_name)
     .Call(C_mark_parsed_expression, x, parse_fun_name)
