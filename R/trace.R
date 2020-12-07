1c6559b12473d98916f9fa923816c8b971720711
Aviral Goel <goel.aviral@gmail.com>
2020-12-07 14:31:14 -0500

    Add write_trace and trace_to_file functions

--- R/trace.R
+++ R/trace.R
@@ -6,7 +6,7 @@
 trace_code <- function(code,
                        envir = parent.frame(),
                        quote=TRUE,
-                       packages = read_system_file("core.txt")) {
+                       packages = read_system_file("corpus.txt")) {
 
     context <- create_tracer(packages)
 
@@ -31,4 +31,54 @@ trace_code <- function(code,
 
 read_system_file <- function(filename) {
     readLines(system.file("extdata", filename, package = "evil"))
+}
+
+
+#' @export
+#' @importFrom instrumentr is_error get_error get_source get_message get_call
+write_trace <- function(traces, writer) {
+
+    if (is_error(traces$result)) {
+        e <- get_error(traces$result)
+
+        error_df <- data.frame(
+            error_source <- get_source(e),
+            error_message <- get_message(e),
+            error_call <- paste(deparse(get_call(e)), sep = "\n")
+        )
+
+        writer("ERROR", error_df)
+    }
+
+    for(table_name in names(traces$tables)) {
+
+        table <- traces$tables[[table_name]]
+        filename <- table_name
+
+        if(is.data.frame(table)) {
+            writer(filename, table)
+        }
+        else {
+            error_df <- data.frame(
+                source = NA,
+                message = paste("expected traces data to be data.frame, found ", sexp_typeof(table)),
+                call = NA
+            )
+            filename <- paste0(filename, "-ERROR")
+            writer(filename, error_df)
+        }
+    }
+
+    invisible(NULL)
+}
+
+#' @export
+trace_to_file <- function(code,
+                          envir = parent.frame(),
+                          quote = TRUE,
+                          packages = read_system_file("corpus.txt"),
+                          writer) {
+    traces <- trace_code(code, envir, quote, packages)
+    write_trace(traces, writer)
+
 }
