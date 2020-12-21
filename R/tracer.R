f92ef2309e7443f871262b0da7cb7d058039e002
Aviral Goel <goel.aviral@gmail.com>
2020-12-21 18:50:09 -0500

    Disable merging

--- R/tracer.R
+++ R/tracer.R
@@ -95,9 +95,9 @@ application_unload_callback <- function(context, application) {
     .Call(C_tracer_data_pop_eval_call, data)
     tables <- .Call(C_tracer_data_finalize, data)
 
-    tables$code <- merge(tables$code, calls, by = "eval_call_id")
+    #tables$code <- merge(tables$code, calls, by = "eval_call_id")
 
-    tables$reflection <- merge(tables$reflection, calls, by = "eval_call_id")
+    #tables$reflection <- merge(tables$reflection, calls, by = "eval_call_id")
 
     data$tables <- c(list(dependencies = dependencies, calls = calls), tables)
 }
