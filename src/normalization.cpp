2a39df16efaf0369050ebfe16292c1a5362b94eb
Jan Vitek <janvite@yahoo.com>
2021-02-05 15:03:43 +0100

    tree work

--- src/normalization.cpp
+++ src/normalization.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+#include <iostream>
 #include "normalization.h"
 #include "r_init.h"
 
@@ -46,7 +48,7 @@ public:
     buf = (char*) malloc(size * sizeof(char));
   }
 
-  ~CharBuff() { free(buf); }
+  // ~CharBuff() { free(buf); }
 
   /* Check if the buffer can take increment new elements, realloc if needed. */
   void growIfNeeded(int increment) {
@@ -165,8 +167,7 @@ public:
   }
 
   NTYPE doSTRSXP(SEXP ast, NTYPE previous, bool merge) {
-    // This comes from the parser and so there is always only one CHARSXP in
-    // the character vector
+    // This comes from the parser, there is one CHARSXP in the vector
     const char* s = CHAR(STRING_ELT(ast, 0));
 
     if (strcmp(s, "<ENVIRONMENT>") == 0) {
@@ -210,13 +211,11 @@ public:
       return normalize(CAR(ptr), previous, 0);
     } else if (fun_name == N_ModelFrame) { // Keep 2 args and subdset
       ptr = CDR(ptr);
-      int i = 0;
-      int isSubset = 0;
+      int i = 0, isSubset = 0;
       buffer.write("(");
       while (ptr != R_NilValue) {
         int old_pos = buffer.pos();
-        const char* arg_name =
-          isNull(TAG(ptr)) ? "NULL" : CHAR(PRINTNAME(TAG(ptr)));
+        const char* arg_name = isNull(TAG(ptr)) ? "NULL" : CHAR(PRINTNAME(TAG(ptr)));
         isSubset = strcmp(arg_name, "subset") == 0;
         if (i < 2 || isSubset) {
           if (isSubset) {
@@ -231,8 +230,7 @@ public:
         if (!isSubset || (ptr != R_NilValue && old_pos != buffer.pos()))
           buffer.write(", ");
       }
-      // It means that subset was not part of the arguments
-      if (!isSubset) {
+      if (!isSubset) { // Subset was not part of the arguments
         if (i < 2)  // One of the two first argument is missing (both can't)
           buffer.write("NULL, ");
         buffer.write("subset = NULL");
@@ -262,30 +260,20 @@ public:
       // To merge all similar elements in a list or vector, or do
       // VAR absorption. We are at the beginning of the list or the
       // first elements were NAs
-      if (ntype == N_NA && fun_name == N_ListVec) {
+      if (ntype == N_NA && fun_name == N_ListVec)
         ntype = ntype_arg;
-      }
       // Different ntypes, excluding VAR so no Constant Folding won't be done
-      if (ntype_arg != ntype && ntype_arg != N_Var && ntype_arg != N_NA) {
+      if (ntype_arg != ntype && ntype_arg != N_Var && ntype_arg != N_NA)
         ntype = N_Other;
-      }
-      if (ntype_arg == N_Var) {
-        // We cannot break out of the loop. VAR will absorb only if all other
-        // arguments are scalar types or VAR (Or constant folded to scalar
-        // types) But we want to keep more elaborated sub-AST
-        isVar = 1;
-      }
+      isVar |= (ntype_arg == N_Var);
+      // We cannot break out of the loop. VAR will absorb only if all other
+      // arguments are scalar types or VAR (Or constant folded to scalar
+       // types) But we want to keep more elaborated sub-AST
       ptr = CDR(ptr);
-      // A bit ugly..., to not write that comma at the end
-      // Another solution would be to rollback the buffer by 2 characters
-      // after the loop But is it efficient? And it also feels hacky...
-      // And we put a comma only if we wrote something after the previous
-      // comma...
-      if (ptr != R_NilValue && old_pos3 != buffer.pos()) {
+      // To avoid writing a  comma at the end.
+      if (ptr != R_NilValue && old_pos3 != buffer.pos())
         buffer.write(", ");
-      }
     }
-
     // Constant folded and VAR absorption. VAR absorption only for special ops.
     if (ntype != N_Other &&
         (fun_name == N_Op || fun_name == N_Logi || fun_name == N_Comp ||
@@ -333,9 +321,267 @@ public:
 };
 
 
+class Tree {
+public:
+  virtual const char* print() { return "";  }
+  virtual bool is_sym() { return false; }
+  virtual bool is_call() { return false; }
+  virtual bool is_null() { return false; }
+  virtual bool is_na() { return false; }
+  virtual bool is_list() { return false; }
+  virtual bool is_statements() { return false; }
+  virtual bool is_num() { return false; }
+  virtual bool is_str() { return false; }
+  virtual bool is_other() { return false; }
+  void write(CharBuff* buf) { buf->write(print()); }
+  virtual Tree* copy() { error("OOPS"); }
+};
+
+class Null : public Tree {
+public:
+  const char* print() { return "NULL"; }
+  bool is_null() { return true; }
+};
+
+class Num : public Tree {
+public:
+  const char* print() { return "0"; }
+};
+
+class Sym : public Tree {
+  const char* name;
+
+public:
+  Sym(const char* nm) {
+    int len = strlen(nm)+1;
+    name =  (char*) malloc(len*sizeof(char)) ;
+    strcpy((char*)name, nm);    
+  }
+
+  ~Sym() { delete name; }
+
+  bool is_sym() { return true; }
+
+  /* All symbols are normalized to 'X'. */
+  const char* print() { return "X"; }
+
+  /* This is used while printing function names. */
+  const char* get_name() { return name; }
+
+};
+
+class NA : public Tree {
+public:
+  const char* print() { return "NA"; }
+};
+
+class Call : public Tree{
+  Sym* name; // name without namespace, could be null
+  Tree* anon; // Expression giving a function (for anon)
+  int kind; // 0=Unknown, 1=model.frame, 2=arith, 3=logic, 4=named
+  std::vector<Tree*> args;
+  const char* string_rep = nullptr;
+  
+public:
+  Call(Sym* nm, Tree* anon) : name(nm), anon(anon) {
+    if (!name) kind = 0;
+    else {
+      const char* str = name->print();
+      if (in(str, arith_op, NB_ARITH_OP))    kind = 2;
+      else if (in(str, bool_op, NB_BOOL_OP)) kind = 3;
+      else if (in(str, cmp_op, NB_COMP_OP))  kind = 3;
+      else if (in(str, str_op, NB_STR_OP))   kind = 3;
+      else if (in(str, listvec, NB_LISTVEC)) kind = 5;//TODO
+      else if (!strcmp(str, "model.frame"))  kind = 1;
+      else                                   kind = 1;
+    }
+  }
+
+  ~Call() {
+    delete anon; // should delete name, since we got name off it in build()
+    if(string_rep) free((char*)string_rep);
+  }
+
+  bool is_call() { return true; }
+
+  bool is_name(const char* nm) {
+    if (!name) return false;
+    return strcmp(nm, name->get_name()) == 0; 
+  }
+
+  const char* get_name() {
+    if (name) return name->get_name();
+    else return anon->print();
+  }
+  
+  const char* print() {
+    if (!string_rep) {
+      CharBuff buf;
+      if(name)
+	buf.write(name->get_name());
+      else
+	anon->write(&buf);
+      buf.write("(");
+      int pos = -1;
+      for(Tree* x : args) {
+	buf.write(x->print());
+	pos = buf.pos();
+	buf.write(", ");
+      }
+      if (pos!=-1) buf.rollback(pos);
+      buf.write(")");
+      string_rep = buf.get();      
+    }
+    return string_rep;
+  }
+
+  void add_arg(Tree* a) { args.push_back(a); }
+
+  Tree* get_arg(int x) { return args[x]; }
+  
+};
+
+class Str : public Tree{
+public:
+  const char* print() { return "CHAR"; }
+};
+
+
+class Statements : public Tree {
+  std::vector<Tree*> elems;
+  const char* string_rep = nullptr;
+public:
+  Statements() {}
+
+  ~Statements() {
+    for(Tree* x : elems) delete x;
+    if (string_rep) free((char*)string_rep);
+  }
+
+  void add(Tree* x) { elems.push_back(x); }
+
+  const char* print() {
+    if (!string_rep) {
+      CharBuff buf;
+      buf.write("{ ");
+      int pos = -1;
+      for(Tree* x : elems) {
+	buf.write(x->print());
+	pos = buf.pos();
+	buf.write("; ");
+      }
+      if (pos!=-1) buf.rollback(pos);
+      buf.write(" }");
+      string_rep = buf.get();      
+    }
+    return string_rep;
+  }
+
+};
+
+class Other : public Tree {
+  const char* name; 
+
+public:
+  Other(const char* nm) { name = nm; }
+
+  ~Other() { /*Do not delete name*/ }
+
+  const char* print() { return name; }
+};
+
+
+
+class Builder {
+
+public:
+  Tree* build(SEXP ast) {
+    switch (TYPEOF(ast)) {
+    case NILSXP:  return new Null();
+    case INTSXP:
+    case REALSXP:
+    case CPLXSXP: return new Num();
+    case SYMSXP:  return  new Sym(CHAR(PRINTNAME(ast)));
+    case LGLSXP:  return asLogical(ast)==NA_LOGICAL ?
+	((Tree*) new NA()) : ((Tree*) new Num());
+    case STRSXP:  return doSTRSXP(ast);
+    case LISTSXP: return doLISTSXP(ast);
+    case LANGSXP: return doLANGSXP(ast);
+    case EXPRSXP: return doEXPRSXP(ast);
+    case DOTSXP:     return doDEFAULT("...");
+    case BCODESXP:   return doDEFAULT("BYTECODE");
+    case RAWSXP:     return doDEFAULT("RAW");
+    case EXTPTRSXP:  return doDEFAULT("PTR");
+    case WEAKREFSXP: return doDEFAULT("WREF");
+    case ENVSXP:     return doDEFAULT("ENV");
+    default:
+      error("Unexpected SEXP: %s!\n", type2char(TYPEOF(ast)));
+    }
+  }
+
+  Tree* doSTRSXP(SEXP ast) {
+    const char* s = CHAR(STRING_ELT(ast, 0));
+    if (!strcmp(s, "<ENVIRONMENT>") || !strcmp(s, "<WEAK REFERENCE>") ||
+        !strcmp(s, "<POINTER>"))
+      return new Other(s);
+    else
+      return new Str();
+  }
+
+  Tree* doLISTSXP(SEXP ast) { return new Other("(Argument list)"); }
+
+  Tree* doLANGSXP(SEXP ast) {
+    Tree* fun = build(CAR(ast)); // Function 'name' can be (1) a
+    Sym* fun_name = nullptr;   // symbol, (2) namespace (langsxp),
+    // (3) anonymous function (langsxp). For (1) and (2) we keep
+    // function names, for (3) we retain the whole expression.
+    if (fun->is_sym()) fun_name = dynamic_cast<Sym*>(fun);
+    else if (fun->is_call()) {  
+      Call* name_call = dynamic_cast<Call*>(fun);
+      if (name_call->is_name("::")) {
+	fun_name = dynamic_cast<Sym*>(name_call->get_arg(1));
+      }
+    } 
+
+    Call* call = new Call(fun_name, fun);
+    // Process the arguments
+    for (SEXP ptr = CDR(ast); ptr != R_NilValue; ptr = CDR(ptr)) {
+      call->add_arg(build(CAR(ptr)));
+    }
+    return call;
+  }
+
+  Tree* doEXPRSXP(SEXP ast) {
+    Statements* stmts = new Statements();
+    int size = Rf_length(ast);
+    for (int i = 0; i < size; i++) 
+      stmts->add(build(VECTOR_ELT(ast, i)));
+    return stmts;
+  }
+
+  Tree* doDEFAULT(const char* val) { return new Other(val);  }
+
+};
+
+
+class Simplifier {
+
+  Tree* simplify(Tree* t) {
+    if
+  }
+  
+};
+
 SEXP r_normalize_expr(SEXP ast) {
   Normalizer norm;
   norm.normalize(ast, N_Other, 0);
+
+  Builder builder;
+  Tree* t = builder.build(ast);
+  CharBuff buf;
+  t->write(&buf);
+  std::cout << buf.get() << std::endl;
+   
   SEXP r_value = PROTECT(mkString(norm.get()));
   UNPROTECT(1);
   return r_value;
