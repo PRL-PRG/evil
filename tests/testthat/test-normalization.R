29c49cefe9c13759e125391f84898e7f9a7195dd
Jan Vitek <janvite@yahoo.com>
2021-02-06 09:03:58 +0100

    Fixing tests

--- tests/testthat/test-normalization.R
+++ tests/testthat/test-normalization.R
@@ -1,20 +1,20 @@
 test_that("Constant folding", {
-  expect_equal(normalize_expr(expression(1 + 1)), "NUM")
-  expect_equal(normalize_expr(quote(1 + 1)), "NUM")
-  expect_equal(normalize_expr(quote(TRUE && FALSE)), "BOOL")
-  expect_equal(normalize_expr(quote(paste0("Hello, ", "World"))), "STR")
-  expect_equal(normalize_expr(quote(x)), "VAR")
-  expect_equal(normalize_expr(quote(x + y)), "OP(VAR)")
-  expect_equal(normalize_expr(quote(x + 1)), "OP(VAR)")
+  expect_equal(normalize_expr(expression(1 + 1)), "0")
+  expect_equal(normalize_expr(quote(1 + 1)), "0")
+  expect_equal(normalize_expr(quote(TRUE && FALSE)), "0")
+  expect_equal(normalize_expr(quote(paste0("Hello, ", "World"))), "\"C\"")
+  expect_equal(normalize_expr(quote(x)), "X")
+  expect_equal(normalize_expr(quote(x + y)), "X")
+  expect_equal(normalize_expr(quote(x + 1)), "X")
 })
 
 test_that("List simplification", {
-  expect_equal(normalize_expr(quote(c(1, 2, 3, 4))), "c(NUM)")
-  expect_equal(normalize_expr(quote(list(1, 2, 3, 4))), "list(NUM)")
-  expect_equal(normalize_expr(quote(c(TRUE, FALSE, FALSE, TRUE))), "c(BOOL)")
-  expect_equal(normalize_expr(quote(c("a", "e", "i", "o"))), "c(STR)")
-  expect_equal(normalize_expr(quote(list(1, "hello"))), "list(NUM, STR)")
-  expect_equal(normalize_expr(quote(c(1, 2, x, 4))), "c(VAR)")
+  expect_equal(normalize_expr(quote(c(1, 2, 3, 4))), "0")
+  expect_equal(normalize_expr(quote(list(1, 2, 3, 4))), "0")
+  expect_equal(normalize_expr(quote(c(TRUE, FALSE, FALSE, TRUE))), "0")
+  expect_equal(normalize_expr(quote(c("a", "e", "i", "o"))), "\"C\"")
+  expect_equal(normalize_expr(quote(list(1, "hello"))), "\"C\"")
+  expect_equal(normalize_expr(quote(c(1, 2, x, 4))), "X")
 })
 
 
@@ -30,54 +30,54 @@ test_that("Stress test", {
       1,  "test", TRUE, "test2",
       1,  "test", TRUE, "test2"
     ))),
-    "list(NUM, STR, BOOL, STR, NUM, STR, BOOL, STR, NUM, STR, BOOL, STR, NUM, STR, BOOL, STR, NUM, STR, BOOL, STR, NUM, STR, BOOL, STR)"
+    "\"C\""
   )
 })
 
 test_that("Anonymous functions", {
-  expect_equal(normalize_expr(quote((function() 1)(2))), "function(NULL, NUM, NUM)(NUM)")
-  expect_equal(normalize_expr(quote((function(a) a + 1)(2))), "function((a), OP(VAR), NUM)(NUM)")
-  expect_equal(normalize_expr(quote((function(a, b) a + b)(2))), "function((a, b), OP(VAR), NUM)(NUM)")
+  expect_equal(normalize_expr(quote((function() 1)(2))), "function(0)(0)")
+  expect_equal(normalize_expr(quote((function(a) a + 1)(2))), "function((ARGS), X)(0)")
+  expect_equal(normalize_expr(quote((function(a, b) a + b)(2))), "function((ARGS), X)(0)")
 })
 
 test_that("Superfluous parenthesis", {
-  expect_equal(normalize_expr(quote((1))), "NUM")
-  expect_equal(normalize_expr(quote((1) + 2)), "NUM")
-  expect_equal(normalize_expr(quote(c(1, (2)))), "c(NUM)")
+  expect_equal(normalize_expr(quote((1))), "0")
+  expect_equal(normalize_expr(quote((1) + 2)), "0")
+  expect_equal(normalize_expr(quote(c(1, (2)))), "0")
 })
 
 test_that("NA coercion", {
   # That can be tricky!
-  expect_equal(normalize_expr(quote(c(1, NA))), "c(NUM)")
-  expect_equal(normalize_expr(quote(c(1, NA, NA))), "c(NUM)")
-  expect_equal(normalize_expr(quote(c(NA, 1))), "c(NUM)")
-  expect_equal(normalize_expr(quote(1 + NA)), "NUM")
-  expect_equal(normalize_expr(quote(NA + 1)), "NUM")
-  expect_equal(normalize_expr(quote(NA + NA + 1)), "NUM")
-  expect_equal(normalize_expr(quote(c(NA))), "c(BOOL)")
-  expect_equal(normalize_expr(quote(NA)), "BOOL")
+  expect_equal(normalize_expr(quote(c(1, NA))), "0")
+  expect_equal(normalize_expr(quote(c(1, NA, NA))), "0")
+  expect_equal(normalize_expr(quote(c(NA, 1))), "0")
+  expect_equal(normalize_expr(quote(1 + NA)), "0")
+  expect_equal(normalize_expr(quote(NA + 1)), "0")
+  expect_equal(normalize_expr(quote(NA + NA + 1)), "0")
+  expect_equal(normalize_expr(quote(c(NA))), "NA")
+  expect_equal(normalize_expr(quote(NA)), "NA")
 })
 
 test_that("model.frame", {
   # Special treatment!
-  expect_equal(normalize_expr(quote(model.frame(x ~ y))), "model.frame(~(VAR, VAR), NULL, subset = NULL)")
-  expect_equal(normalize_expr(quote(model.frame(x ~ y, NULL))), "model.frame(~(VAR, VAR), NULL, subset = NULL)")
-  expect_equal(normalize_expr(quote(model.frame(x ~ y, subset = t))), "model.frame(~(VAR, VAR), NULL, subset = VAR)")
+  expect_equal(normalize_expr(quote(model.frame(x ~ y))), "model.frame(~(X))")
+  expect_equal(normalize_expr(quote(model.frame(x ~ y, NULL))), "model.frame(~(X), NULL)")
+  expect_equal(normalize_expr(quote(model.frame(x ~ y, subset = t))), "model.frame(~(X), X)")
 })
 
 test_that("Various normalization", {
   # Crushing consecutive same types in c and list1
   expect_equal(
     normalize_expr(quote(c(1, 2, 3, "hi", "test", 4, "true"))),
-    "c(NUM, STR, NUM, STR)"
+    "\"C\""
   )
 
   # Namespace name is ignored
   expect_equal(
-    normalize_expr(quote(a::b(4))), "b(NUM)"
+    normalize_expr(quote(a::b(4))), "b(0)"
   )
 
   expect_equal(
-    normalize_expr(quote(rgt::`+`(1, 2))), "NUM"
+    normalize_expr(quote(rgt::`+`(1, 2))), "0"
   )
 })
