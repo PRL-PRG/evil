148048028d2a0de820b21fca2b2cc9d2fa8f5329
Jan Vitek <janvite@yahoo.com>
2021-02-18 15:20:00 +0100

    changes

--- R/normalization.R
+++ R/normalization.R
@@ -1,24 +1,4 @@
 #' @export
-normalize_expr <- function(expr) {
-    .Call(C_normalize_expr, expr)
-}
-
-#' @export
-build_tree <- function(expr) {
-    .Call(C_build_tree, expr)
-}
-
-#' @export
-tree_to_string <- function(expr) {
-    .Call(C_tree_to_string, expr)
-}
-
-#' @export
-simplify_tree <- function(expr) {
-    .Call(C_simplify, expr)
-}
-
-#' @export
-normalize_stats_expr <- function(expr) {
-    .Call(C_normalize_stats_expr, expr)
+normalize <- function(h, expr) {
+    .Call(C_normalize, h, expr)
 }
