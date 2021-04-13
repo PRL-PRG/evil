ac13eeeac8d21d8bf298cc86f58eb16a81269a21
programLyrique <pierre.donat.bouillud@gmail.com>
2021-04-13 17:44:01 -0400

    Tests for detection of match.call even for elements in the call list

--- tests/testthat/test-matchcall.R
+++ tests/testthat/test-matchcall.R
@@ -16,3 +16,36 @@ test_that("match.call is well detected", {
   expect_false(is.na(calls$expr_match_call))
   expect_equal(calls$expr_match_call, "match.call")
 })
+
+
+test_that("match.call and symbols, scalars", {
+  g <- function(a1, a2) {
+      mf <- match.call()
+      eval(mf[[2]], parent.frame()) 
+  }
+
+  f <- function() {
+    x <- 1
+    g(x, 3)
+  }
+
+  calls <- do_trace_eval(f())
+
+  expect_equal(calls$expr_match_call, "match.call")
+
+  f <- function() {
+    g(1, 3)
+  }
+
+  calls <- do_trace_eval(f())
+
+  expect_equal(calls$expr_match_call, "match.call")
+
+  f <- function() {
+    g("hello", 3)
+  }
+
+  calls <- do_trace_eval(f())
+
+  expect_equal(calls$expr_match_call, "match.call")
+})
