1ff4f30ce8b385d1eae6a6cf281e4d4f442b23ee
programLyrique <pierre.donat.bouillud@gmail.com>
2021-07-20 10:45:35 -0400

    More tests

--- tests/testthat/test-provenance.R
+++ tests/testthat/test-provenance.R
@@ -221,3 +221,73 @@ test_that("attributes", {
     expect_equal(provs$provenance_args, "parse(text = \"1 ; a\")")
     expect_equal(provs$nb_provenances, 1) 
 })
+
+
+test_that("expression in a list", {
+    g <- function() {
+        convergence <- expression({
+            a + 1
+        })
+
+        list(p = 45, convergence = convergence)
+    }
+
+    provs <- do_trace_provenances({
+        a <- 3
+
+        l <- g()
+
+        eval(l$convergence)
+    })
+
+    expect_equal(provs$provenance, "expression")
+    expect_equal(provs$provenance_args, "expression({\\n     a + 1\\n })")
+    expect_equal(provs$nb_provenances, 1) 
+})
+
+test_that("expression in default argument", {
+    g <- function(b = expression(a+1)) {
+        list(p = 45, ex = b)
+    }
+
+    provs <- do_trace_provenances({
+        a <- 3
+
+        l <- g()
+
+        eval(l$ex)
+    })
+
+    expect_equal(provs$provenance, "expression")
+    expect_equal(provs$provenance_args, "expression(a + 1)")
+    expect_equal(provs$nb_provenances, 1) 
+})
+
+test_that("structure", {
+    provs <- do_trace_provenances({
+        a <- 3
+
+        l <- structure(list(conver = quote(a+1)), plop = "test")
+
+        eval(l$conver)
+    })
+
+    expect_equal(provs$provenance, "quote")
+    expect_equal(provs$provenance_args, "quote(a + 1)")
+    expect_equal(provs$nb_provenances, 1) 
+})
+
+test_that("slot", {
+    provs <- do_trace_provenances({
+        setClass("langObj", slots = c(x="expression", y="numeric"))
+        myLang <- new("langObj", x = expression(a + 1), y = exp(-4:4))
+        a <- -9
+        eval(slot(myLang, "x"))
+    })
+
+    provs <- provs[3,]
+
+    expect_equal(provs$provenance, "expression")
+    expect_equal(provs$provenance_args, "expression(a + 1)")
+    expect_equal(provs$nb_provenances, 1) 
+})
